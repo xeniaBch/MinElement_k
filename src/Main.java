e71439a12d556334eed363fdd9a73a3304a0b326
xeniaBch <kseniabekhtgold@gmail.com>
2022-11-01 18:50:21 +0100

    HW second variant

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] array = {1, -1, -2, 3};
         minFour2(array);
+        minFour(array);
     }
 
     private static void minFour2(int[] array) {
@@ -44,5 +47,40 @@ public class Main {
         }
         System.out.println("min3 = " + min3);
         System.out.println("min4 = " + min4);
+    }
+
+    private static void minFour(int[] inputArr) {
+        int[] minArr = new int[4];
+        Arrays.fill(minArr, Integer.MAX_VALUE);
+
+        for (int el : inputArr) {
+            int count = 0;
+
+            for (int elMin : minArr) {
+                if (el == elMin) break;
+
+                if (el < elMin) {
+                    for (int i = minArr.length - 1; i > count; i--) {
+                        minArr[i] = minArr[i - 1];
+                    }
+                    minArr[count] = el;
+                    break;
+                }
+                count++;
+            }
+        }
+
+        if (minArr[2] == Integer.MAX_VALUE) {
+            System.out.println("min3 doesn't exist");
+            System.out.println("min4 doesn't exist");
+            return;
+        }
+        if (minArr[3] == Integer.MAX_VALUE) {
+            System.out.println("min3 = " + minArr[2]);
+            System.out.println("min4 doesn't exist");
+            return;
+        }
+        System.out.println("min3 = " + minArr[2]);
+        System.out.println("min4 = " + minArr[3]);
     }
 }
